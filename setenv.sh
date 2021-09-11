e7c3453f2e02706af5678766e4bf280d34ffab26
Yongkang He <he@yongkang.cloud>
2021-09-11 20:30:24 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,9 +1,9 @@
 #-------Set the environment variables"
-export MY_GROUP=rg4yong1 #Change to your favorite resource group name
-export MY_CLUSTER=aks4yong1 #Change to your favorite cluster name
-export MY_VMSIZE=Standard_D2as_v4 #Change to your favorite machine type
-export MY_LOCATION=centralindia #Change to your favorite location
-export MY_REGION="Central India" #Change to your favorite region for Blob Storage
-export MY_CONTAINER=k10container4yong1 #Change to your favorite bucket, unique name in your project
-export MY_OBJECT_STORAGE_PROFILE=myazblob1 #Change to your favorite profile name
-
+export MY_GROUP=rg4yong1 #Customize your resource group name
+export MY_CLUSTER=aks4yong1 #Customize your cluster name
+export MY_VMSIZE=Standard_D2as_v4 #Customize your VM size
+export MY_LOCATION=centralindia #Customize your location
+export MY_REGION="Central India" #Customize region for Blob Storage
+export MY_CONTAINER=k10container4yong1 #Customize your container
+export MY_OBJECT_STORAGE_PROFILE=myazblob1 #Customize your profile name
+export AZURE_STORAGE_ACCOUNT_ID=azsa4yong1 #Customize your Storage Account
