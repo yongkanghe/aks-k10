6c9cf99608576ef36603a20b8871f2af6f9ac830
Yongkang He <he@yongkang.cloud>
2021-09-11 20:10:15 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -2,12 +2,12 @@ starttime=$(date +%s)
 . setenv.sh
 echo '-------Deleting an AKS Cluster (typically in less than 10 mins)'
 
-az group delete -g $MY_GROUP --yes
+az group delete -g $MY_PREFIX-$MY_GROUP --yes
 kubectl config delete-context $(kubectl config get-contexts | grep $MY_CLUSTER | awk '{print $2}')
 
 echo '-------Deleting the storage container & app registration'
-az storage account delete -n MY_PREFIX-$AZURE_STORAGE_ACCOUNT_ID -g $MY_GROUP --yes
-MYID=$(az ad sp list --show-mine --query [].servicePrincipalNames -o table | grep $MY_GROUP | awk '{print $2}')
+az storage account delete -n MY_PREFIX-$AZURE_STORAGE_ACCOUNT_ID -g $MY_PREFIX-$MY_GROUP --yes
+MYID=$(az ad sp list --show-mine --query [].servicePrincipalNames -o table | grep $MY_PREFIX-$MY_GROUP | awk '{print $2}')
 az ad app delete --id $MYID
 
 endtime=$(date +%s)
