8842f349792cf3befe50c7b27f64dc0fe8470a14
Yongkang He <he@yongkang.cloud>
2021-09-11 21:07:44 +0800

    Updated

--- destroy.sh
+++ destroy.sh
@@ -5,8 +5,8 @@ MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print t
 az group delete -g $MY_PREFIX-$MY_GROUP --yes
 kubectl config delete-context $(kubectl config get-contexts | grep $MY_CLUSTER | awk '{print $2}')
 
-echo '-------Deleting the storage container & app registration'
-az storage account delete -n MY_PREFIX-$AZURE_STORAGE_ACCOUNT_ID -g $MY_PREFIX-$MY_GROUP --yes
+echo '-------Deleting the app registration created by AKS'
+#az storage account delete -n MY_PREFIX-$AZURE_STORAGE_ACCOUNT_ID -g $MY_PREFIX-$MY_GROUP --yes
 MYID=$(az ad sp list --show-mine --query [].servicePrincipalNames -o table | grep $MY_PREFIX-$MY_GROUP | awk '{print $2}')
 az ad app delete --id $MYID
 
