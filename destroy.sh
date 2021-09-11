3fac9ca6dd8241f97e9b271044ed6d783ecdf7d1
Yongkang He <he@yongkang.cloud>
2021-09-11 19:39:49 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,14 +1,9 @@
 starttime=$(date +%s)
 . setenv.sh
-echo '-------Deleting a GKE Cluster (typically in less than 10 mins)'
-gkeclustername=$(gcloud container clusters list | grep $MY_CLUSTER | awk '{print $1}')
-gcloud container clusters delete $gkeclustername --zone $MY_ZONE --quiet
+echo '-------Deleting an AKS Cluster (typically in less than 10 mins)'
 
-echo '-------Deleting disks'
-for i in $(gcloud compute disks list --filter="zone:($MY_ZONE)" | grep $MY_CLUSTER | awk '{print $1}');do echo $i;gcloud compute disks delete $i --zone=$MY_ZONE -q;done
-
-echo '-------Deleting snapshots'
-for i in $(gcloud compute snapshots list | grep $MY_CLUSTER | awk '{print $1}');do echo $i;gcloud compute snapshots delete $i -q;done
+az group delete -g $MY_GROUP --yes
+kubectl config delete-context $(kubectl config get-contexts | grep $MY_CLUSTER | awk '{print $2}')
 
 echo '-------Deleting objects from the bucket'
 myproject=$(gcloud config get-value core/project)
@@ -17,3 +12,6 @@ gsutil -m rm -r gs://$myproject-$MY_BUCKET/k10
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
 echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
