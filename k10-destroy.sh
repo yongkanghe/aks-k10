3bb83e92c98f432a98f3465066a03d305367814a
Yongkang He <he@yongkang.cloud>
2022-07-16 16:28:39 +0800

    Update k10-destroy.sh

--- k10-destroy.sh
+++ k10-destroy.sh
@@ -10,6 +10,7 @@ kubectl delete ns kasten-io
 echo '-------Deleting objects from Azure Blob Storage Bucket'
 az storage blob delete-batch --account-name $MY_PREFIX$AZURE_STORAGE_ACCOUNT_ID -s $MY_PREFIX-$MY_CONTAINER --account-key $(cat az_storage_key)
 
+echo "" | awk '{print $1}'
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
 echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
