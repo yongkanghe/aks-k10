ac56f7f297e2aeb846f78a3eb043f4d74f6378b4
Yongkang He <he@yongkang.cloud>
2022-07-16 15:07:11 +0800

    Create k10-deploy.sh

---
+++ k10-deploy.sh
@@ -0,0 +1,90 @@
+echo '-------Deploying Kasten K10 and MySQL'
+starttime=$(date +%s)
+. ./setenv.sh
+
+echo '-------Exporting the Azure Tenant, Client, Secret'
+AZURE_TENANT_ID=$(cat aks4yong1app | grep tenant | awk '{print $2}' | sed -e 's/\"//g')
+AZURE_CLIENT_ID=$(cat aks4yong1app | grep appId | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/\,//g')
+AZURE_CLIENT_SECRET=$(cat aks4yong1app | grep password | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/\,//g')
+
+echo '-------Install K10'
+kubectl create ns kasten-io
+helm repo add kasten https://charts.kasten.io/
+helm repo update 
+
+#For Production, remove the lines ending with =1Gi from helm install
+#For Production, remove the lines ending with airgap from helm install
+helm install k10 kasten/k10 --namespace=kasten-io \
+  --set secrets.azureTenantId=$AZURE_TENANT_ID \
+  --set secrets.azureClientId=$AZURE_CLIENT_ID \
+  --set secrets.azureClientSecret=$AZURE_CLIENT_SECRET \
+  --set global.persistence.metering.size=1Gi \
+  --set prometheus.server.persistentVolume.size=1Gi \
+  --set global.persistence.catalog.size=1Gi \
+  --set global.persistence.jobs.size=1Gi \
+  --set global.persistence.logging.size=1Gi \
+  --set global.persistence.grafana.size=1Gi \
+  --set auth.tokenAuth.enabled=true \
+  --set externalGateway.create=true \
+  --set metering.mode=airgap 
+
+echo '-------Set the default ns to k10'
+kubectl config set-context --current --namespace kasten-io
+
+echo '-------Creating an azure disk vsc'
+cat <<EOF | kubectl apply -f -
+apiVersion: snapshot.storage.k8s.io/v1
+kind: VolumeSnapshotClass
+metadata:
+  annotations:
+    k10.kasten.io/is-snapshot-class: "true"
+  name: csi-azuredisk-vsc
+driver: disk.csi.azure.com
+deletionPolicy: Delete
+parameters:
+  incremental: "true"
+EOF
+
+echo '-------Deploy a MySQL database'
+kubectl create namespace yong-mysql
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install mysql bitnami/mysql --namespace=yong-mysql \
+  --set primary.persistence.size=1Gi,secondary.persistence.size=1Gi	
+
+echo '-------Create a Azure Storage account'
+AKS_RG=$(az group list -o table | grep $MY_PREFIX-$MY_GROUP | grep MC | awk '{print $1}')
+az storage account create -n $MY_PREFIX$AZURE_STORAGE_ACCOUNT_ID -g $AKS_RG -l $MY_LOCATION --sku Standard_LRS
+export AZURE_STORAGE_KEY=$(az storage account keys list -g $AKS_RG -n $MY_PREFIX$AZURE_STORAGE_ACCOUNT_ID --query [].value -o tsv | head -1)
+
+echo '-------Waiting for the Cluster ID, Web UI IP and token in about 2 mins'
+clusterid=$(kubectl get namespace default -ojsonpath="{.metadata.uid}{'\n'}")
+echo "" | awk '{print $1}' > aks-token
+echo My Cluster ID is $clusterid >> aks-token
+kubectl wait --for=condition=ready --timeout=180s -n kasten-io pod -l component=jobs
+k10ui=http://$(kubectl get svc gateway-ext | awk '{print $4}'|grep -v EXTERNAL)/k10/#
+echo -e "\nHere is the URL to log into K10 Web UI" >> aks-token
+echo -e "\n$k10ui" >> aks-token
+echo "" | awk '{print $1}' >> aks-token
+sa_secret=$(kubectl get serviceaccount k10-k10 -o jsonpath="{.secrets[0].name}" --namespace kasten-io)
+echo "Here is the token to login K10 Web UI" >> aks-token
+echo "" | awk '{print $1}' >> aks-token
+kubectl get secret $sa_secret --namespace kasten-io -ojsonpath="{.data.token}{'\n'}" | base64 --decode | awk '{print $1}' >> aks-token
+echo "" | awk '{print $1}' >> aks-token
+
+echo '-------Waiting for K10 services are up running in about 3 mins more or less'
+kubectl wait --for=condition=ready --timeout=300s -n kasten-io pod -l component=catalog
+
+./az-location.sh
+
+./mysql-policy.sh
+
+echo '-------Accessing K10 UI'
+cat aks-token
+
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "-------Total time for K10 deployment is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "" | awk '{print $1}'
